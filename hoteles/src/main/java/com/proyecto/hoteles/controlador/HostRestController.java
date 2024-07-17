d1138c560cda4525368853b69f2b32c720b24b9a
andresshm <andreshrtd07@gmail.com>
2024-07-17 11:47:29 +0200

    cross origins

--- hoteles/src/main/java/com/proyecto/hoteles/controlador/HostRestController.java
+++ hoteles/src/main/java/com/proyecto/hoteles/controlador/HostRestController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -26,6 +27,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Host API", description = "Esta API sirve para gestionar los huéspedes")
 @RestController
+@CrossOrigin(origins = "*"/*"http://localhost:4200" */)
 @RequestMapping("/huesped")
 public class HostRestController {
     @Autowired
@@ -40,6 +42,7 @@ public class HostRestController {
      * @return
      */
     @Operation(summary = "Devuelve una lista con todos los huéspedes")
+ 
     @GetMapping()
     public ResponseEntity<?> findAll() {
         return servicio.getAll();
